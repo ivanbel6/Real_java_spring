153923eeb7a178cf252f9fa6a6d0304fe9c8fce9
ivanbel6 <bel-ivan03@mail.ru>
2023-10-13 14:52:13 +0300

    Удаление готово

--- src/main/java/com/example/real_java_spring/controller/CartController.java
+++ src/main/java/com/example/real_java_spring/controller/CartController.java
@@ -6,17 +6,19 @@ import com.example.real_java_spring.model.Product;
 import com.example.real_java_spring.service.CartItemService;
 import com.example.real_java_spring.service.CartService;
 import com.example.real_java_spring.service.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/cart")
+@Controller
 public class CartController {
     private final CartService cartService;
     private final ProductService productService;
     private final CartItemService cartItemService;
-
+    @Autowired
     public CartController(CartService cartService, ProductService productService, CartItemService cartItemService) {
         this.cartService = cartService;
         this.productService = productService;
@@ -54,11 +56,12 @@ public class CartController {
     }
 
     @PostMapping("/delete/{cartItemId}")
-    public String deleteCartItem(@PathVariable Long cartItemId) {
+    public String deleteCartItem(@PathVariable Long cartItemId, HttpServletRequest request) {
         CartItem cartItem = cartItemService.getCartItemById(cartItemId);
-        Long cartId = cartItem.getCart().getId();  // we get the id of the cart before deleting the item
+        Long cartId = cartItem.getCart().getUser().getId();  // we get the id of the cart before deleting the item
         cartItemService.deleteCartItem(cartItem);
-        return "redirect:/user-cart/" + cartId; // Removed the extra "<" character in the redirected URL
+        String referer = request.getHeader("Referer");
+        return "redirect:" + referer.substring(0, referer.lastIndexOf('/') + 1) + cartId;
     }
 
 
