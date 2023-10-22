72a5d3a0c8d5c836e412e82149a5b693261e34e9
ivanbel6 <bel-ivan03@mail.ru>
2023-10-22 21:49:13 +0300

    _3_

--- src/main/java/com/example/real_java_spring/model/User.java
+++ src/main/java/com/example/real_java_spring/model/User.java
@@ -2,38 +2,30 @@ package com.example.real_java_spring.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import org.springframework.data.annotation.Id;
 
-
-@Data //lam book - getters и setters
+@Data //lombok - getters и setters
 @Entity
 @Table(name = "users")
 public class User {
-    @Getter
     @OneToOne(cascade = CascadeType.ALL)
     private Cart cart;
 
-    // Getters and setters
-
-    public void setCart(Cart cart) {
-        this.cart = cart;
-    }
-    @Getter
     @jakarta.persistence.Id
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "email")
     private String email;
+
     @Column(name = "age")
     private int age;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Column(name = "role")
+    private String role;
 
+    @Column(name = "password")
+    private String password;
 }
-
